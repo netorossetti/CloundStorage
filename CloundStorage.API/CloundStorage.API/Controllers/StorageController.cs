ed04f91b9fb23d2ef9ee79afdb4d225699122b66
Neto Rossetti <netinho.rossetti@gmail.com>
2024-03-12 19:59:11 -0300

    Add project files.

---
+++ CloundStorage.API/CloundStorage.API/Controllers/StorageController.cs
@@ -0,0 +1,17 @@
+﻿using CloundStorage.Application.UseCases.Users.UploadProfilePhoto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloundStorage.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController : ControllerBase {
+
+    [HttpPost]
+    public IActionResult UploadImage(IFormFile file) {
+
+        var useCase = new UploadProfilePhotoUseCase();
+        useCase.Execute(file);
+        return Created();
+    }
+}
---
+++ CloundStorage.Application/CloundStorage.Application/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
@@ -0,0 +1,15 @@
+﻿using FileTypeChecker;
+using FileTypeChecker.Extensions;
+using FileTypeChecker.Types;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace CloundStorage.Application.UseCases.Users.UploadProfilePhoto;
+public class UploadProfilePhotoUseCase {
+    public void Execute(IFormFile file) {
+        var fileStream = file.OpenReadStream();
+        var isImage = fileStream.Is<JointPhotographicExpertsGroup>();
+
+
+    }
+}
